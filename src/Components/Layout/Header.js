0b1594c6a9ccff2dd7182ece5981105067715f39
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-05-20 17:18:04 +0530

    food task3

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,19 +2,18 @@ import { Fragment } from "react";
 import mealPic from '../../assets/foodPic.jpg';
 import classes from '../Layout/Header.module.css';
 import HeaderCartButton from "./HeaderCartButton";
-import MealSummary from "../Meals/MealSummary";
+
 
 const Header = (props)=>{
 return(
     <Fragment>
         <header className= {classes.header}>
-        <h2>Bhojan</h2>
+        <h2>PehlaNiwala</h2>
         <HeaderCartButton   />
         </header>
         <div className={classes['main-image']}>
          <img src = {mealPic} alt=" Variety of food"/>
         </div>
-        <div><MealSummary /></div>
     </Fragment>
 )
 }
